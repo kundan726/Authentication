204bdd40892f37bafdc79542536e328acc5587e9
kundan726 <kkrs1998@gmail.com>
2023-12-30 21:49:37 +0530

    FEATURE:

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,9 @@ import userController from '../controllers/userController.js';
 
 //Public Route
 console.log("44444444444444")
-router.post('/register', userController.userRegistration)
+router.post('/register', userController.userRegistration);
+router.post('/login', userController.userLogin)
+
 
 //Protected Route
 
